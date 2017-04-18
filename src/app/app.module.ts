e997be941f28c33624ee8f3d88605291b2bb72be
ThomasAnkcorn <thomasankcorn@gmail.com>
2017-04-18 23:50:51 +0100

    Scaffolded application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,25 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LocationComponent } from './location/location.component';
+import { DisruptionListComponent } from './disruption-list/disruption-list.component';
+import { DisruptionComponent } from './disruption/disruption.component';
 
+import { GeoService } from './geo.service'
+import { TflService } from './tfl.service'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LocationComponent,
+    DisruptionListComponent,
+    DisruptionComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [GeoService,TflService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
