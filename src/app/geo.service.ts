a495ef5023c339b9e804d01faa05565ce1a38b18
ThomasAnkcorn <thomasankcorn@gmail.com>
2017-04-19 00:35:07 +0100

    Adding services

--- src/app/geo.service.ts
+++ src/app/geo.service.ts
@@ -1,8 +1,52 @@
 import { Injectable } from '@angular/core';
 
+import { Http, Response }          from '@angular/http';  //Standard Http libraries that are part of Angular
+
+import { Observable } from 'rxjs/Rx';  //An API for asynchronous programming with observable streams
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+
 @Injectable()
 export class GeoService {
 
-  constructor() { }
+  //API Endpoint and google api key
+  BASEURL = 'https://maps.googleapis.com/maps/api/geocode/json?address='
+  Key = '&key=AIzaSyDAXOkRTBYTZWK_lM1BUclDbGn1BqFUj7c'
+
+  //Initialize a private instance of the Http class
+  constructor(private http:Http) { }
+
+  //This function makes 2 simultaneous http get requests using the rxjs library
+  getLatLong(place0:string,place1:string):Observable<string[]>{
+   return Observable.forkJoin(
+      this.http.get(this.BASEURL+place1+this.Key).map(this.extractData).catch(this.handleError),
+      this.http.get(this.BASEURL+place0+this.Key).map(this.extractData).catch(this.handleError)
+    )
+  }
+
+  //Extra the data and format it as the result string the tfl api needs
+  private extractData(res: Response):string {
+    let body = res.json();
+    let location = body.results[0].geometry.location.lat+'%2C%20'+body.results[0].geometry.location.lng
+    console.log(location)
+    return location
+  }
+
+
+  //Handle the error (This is just copied off the internet... shhhh )
+  private handleError (error: Response | any) {
+    // In a real world app, you might use a remote logging infrastructure
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
+  }
 
 }
+
--- src/app/tfl.service.ts
+++ src/app/tfl.service.ts
@@ -1,8 +1,65 @@
 import { Injectable } from '@angular/core';
 
+import { Http, Response }          from '@angular/http'; //Standard Http libraries that are part of Angular
+
+import { Observable } from 'rxjs/Observable'; //An API for asynchronous programming with observable streams
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+
 @Injectable()
 export class TflService {
+ 
+  //The tfl Api endpoint
+  Base_URL:string = 'https://api.tfl.gov.uk/Journey/JourneyResults/'
 
-  constructor() { }
+  //Construct a private instance of the Http class
+  constructor(private http:Http) { }
 
+
+  //Too understand these more head too https://angular.io/docs/ts/latest/cookbook/component-communication.html#bidirectional-service
+  private requestSource = new Subject<boolean>(); // 
+  requestComplete$ = this.requestSource.asObservable(); //Lets everyone know the request is completed
+
+  //makes a get request with the location data requested from googles geo api
+  getDisruptionData(from:string,to:string){
+    this.http.get(this.Base_URL+from+'/to/'+to)
+        .map(this.extractDisruption)
+        .catch(this.handleError)
+        .subscribe((data)=>{    //We subscribe to the event here so that it can be requested from somewhere other than the destination of the data
+            this.requestSource.next(data) //This fires an event containing the response object and lets the rest of the application know the tfl request has successfully returned results
+        })
+  }
+
+//Formats the json object returned so it contains only what we need
+extractDisruption(res:Response):Object[]{
+  let data = res.json()
+  let array = []
+   
+
+    //Im so sorry (Thanks tfl for this wonderful response object)
+    data.journeys.map(function(obj){
+      obj.legs.map(function(obj){
+         obj.disruptions.map(function(obj){
+           //if(obj.category!=='Information'){ //You can filter out categorys
+           array.push(obj)//}
+         })
+      })})
+     return array
+}
+
+//Handle the error (This is just copied off the internet... shhhh )
+private handleError (error: Response | any) {
+    // In a real world app, you might use a remote logging infrastructure
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
+  }
 }
