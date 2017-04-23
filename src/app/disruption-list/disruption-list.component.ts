853e7f4f5572aec2096203e5181eebffcea84c12
ThomasAnkcorn <thomasankcorn@gmail.com>
2017-04-23 21:35:52 +0100

    Loaded results array

--- src/app/disruption-list/disruption-list.component.ts
+++ src/app/disruption-list/disruption-list.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
 
+import { TflService } from '../tfl.service'
 @Component({
   selector: 'app-disruption-list',
   templateUrl: './disruption-list.component.html',
-  styleUrls: ['./disruption-list.component.css']
+  styleUrls: ['./disruption-list.component.css'],
 })
+
 export class DisruptionListComponent implements OnInit {
 
-  constructor() { }
+  constructor(@Inject(TflService) private tfl) { 
+    this.tfl.requestComplete$.subscribe((tflRouteData) => {
+      console.log('Data request was made')
+      this.disruptionList = tflRouteData
+
+    })
+  }
+ 
+  disruptionList = [];
+ 
+
 
   ngOnInit() {
   }
+
+
 
 }
