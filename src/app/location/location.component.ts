11ff8b5fa801f99304cd3f14cd8eb474d4f84297
ThomasAnkcorn <thomasankcorn@gmail.com>
2017-04-23 14:02:12 +0100

    Created databindings between location template and its component

--- src/app/location/location.component.ts
+++ src/app/location/location.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./location.component.css']
 })
 export class LocationComponent implements OnInit {
+  from:string
+  to:string
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  search(){
+    console.log(this.from,this.to)
+  }
 }
