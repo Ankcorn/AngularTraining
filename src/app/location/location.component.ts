007b0c1ba40ba656f98603bdefafff0df875e5c4
ThomasAnkcorn <thomasankcorn@gmail.com>
2017-04-23 21:30:14 +0100

    Connected location component to the services

--- src/app/location/location.component.ts
+++ src/app/location/location.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+
+import { GeoService } from '../geo.service'
+import { TflService } from '../tfl.service'
 
 @Component({
   selector: 'app-location',
@@ -6,15 +9,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./location.component.css']
 })
 export class LocationComponent implements OnInit {
+
+  constructor(@Inject(GeoService) private geo,@Inject(TflService) private tfl) { }
   from:string
   to:string
 
-  constructor() { }
+  searchButtonHTML = "Search";
 
-  ngOnInit() {
+  ngOnInit(){
   }
 
+  fromInput($event:any){
+      this.from = $event.target.value
+  }
+  toInput($event:any){
+      this.to = $event.target.value
+  }
   search(){
-    console.log(this.from,this.to)
+    this.searchButtonHTML = '<i class="fa fa-circle-o-notch fa-spin fa-3x fa-fw"></i>'; //Just a litle bit of sugar
+
+    //Get the start and end locations then send them to the tfl api
+    this.geoLocation(this.to,this.from,(tolocation,fromlocation)=>{
+      //This is the body of the callback function
+        this.tfl.getData(fromlocation,tolocation)
+            this.searchButtonHTML = "Search";
+
+    })
+  }
+
+  geoLocation(to:string,from:string,callback){
+    //makes requests for both locations latitude and longitude and returns the results in an array
+    this.geo.getLatLong(to,from).subscribe((result) =>{
+      //Once complete pass to the callback
+      callback(result[0],result[1])
+    })
+
   }
 }
